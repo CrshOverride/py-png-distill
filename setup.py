aeff783fb754fb282529793a329d165776f9bdae
Justin Niessner <justin.niessner@adroll.com>
2015-10-03 23:32:41 -0400

    Move the source files to the src directory and hope things still work

--- setup.py
+++ setup.py
@@ -67,6 +67,8 @@ setup(
     # What does your project relate to?
     keywords='png development image optimization',
 
+    package_dir={'': 'src'},
+
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
